4ea09a648d6b7fd2dab3e15a383a6ebabcd12b9e
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-04 12:43:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from "react";
 import Header from "./Components/Header";
 import Generator from "./Components/Generator";
 import Footer from "./Components/Footer";
+import { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
-    <div className="bg-[#E9F6FF] w-full min-h-screen">
-      <Header />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
       <Generator />
       <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
