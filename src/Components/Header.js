4a8c8a57ba7e6e8a30b1c943004e188f74bc48cb
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-04 05:14:04 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { blue } from '@mui/material/colors';
 
 function Header() {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" bgcolor="#FE7A36" color="#49243E" p={0} width="100%">
+    <Box display="flex" justifyContent="center" alignItems="center" bgcolor="#FE7A36" color="#49243E" p={0} width='100%' boxShadow={2}>
       <Typography className='Header' variant="h6" sx={{ fontFamily: 'Raleway, sans-serif', color: 'cyan' }} gutterBottom>
         Passforge - select and copy 
       </Typography>
