956ace2d6458465c290e24e0d8ebe99cbf4182c3
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-04 12:42:50 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { IconButton, Tooltip } from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Grid from '@mui/material/Grid';
 
-function Header() {
+function Header({ darkMode, setDarkMode }) {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" bgcolor="#FE7A36" color="#49243E" p={0} width='100%' boxShadow={2}>
-      <Typography className='Header' variant="h6" sx={{ fontFamily: 'Raleway, sans-serif', color: 'cyan' }} gutterBottom>
-        Passforge - select and copy 
-      </Typography>
+    <Box display="flex" justifyContent="space-between" alignItems="center" bgcolor="#FE7A36" color="#49243E" p={0} width='100%' boxShadow={2}>
+      <Grid item xs>
+        <Typography className='Header' variant="h6" sx={{ fontFamily: 'Raleway, sans-serif', color: 'cyan', marginLeft: 1 }} gutterBottom>
+          Passforge - select and copy 
+        </Typography>
+      </Grid>
+      <Grid item xs={1}>
+        <Tooltip title="Toggle dark mode">
+          <IconButton color="inherit" onClick={() => setDarkMode(!darkMode)}>
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </Tooltip>
+      </Grid>
     </Box>
   );
 }
