39392fac95e47bf93caa29693827e27a17bbdbd1
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-06 13:45:24 +0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import { FaLinkedin, FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Paper } from '@mui/material';
 
 function Footer() {
     return (
@@ -12,20 +12,22 @@ function Footer() {
             // position="fixed"
             // bottom={0}
         >
-            <div className='flex flex-row justify-center items-center w-full'>
-                <IconButton color="inherit" target='_blank'  rel="noopener noreferrer" href='https://www.linkedin.com/in/abdullahtariq78/'>
-                    <FaLinkedin size={24} />
-                </IconButton>
-                <IconButton color="inherit" target='_blank' rel="noopener noreferrer" href='https://www.facebook.com/abdullah.tariq.7262'>
-                    <FaFacebook size={24} />
-                </IconButton>
-                <IconButton color="inherit" href="https://www.instagram.com/abdullahtariq62/?next=%2F&hl=en" target="_blank" rel="noopener noreferrer">
-                    <FaInstagram size={24} />
-                </IconButton>
-                <IconButton color="inherit" target='blank' rel="noopener noreferrer" href='https://github.com/Abdullahprogramme'>
-                    <FaGithub size={24} />
-                </IconButton>
-            </div>
+            <Paper elevation={3} sx={{ m: 0.7 }}>
+                <div className='flex flex-row justify-center items-center w-full'>
+                    <IconButton color="inherit" target='_blank'  rel="noopener noreferrer" href='https://www.linkedin.com/in/abdullahtariq78/'>
+                        <FaLinkedin size={24} />
+                    </IconButton>
+                    <IconButton color="inherit" target='_blank' rel="noopener noreferrer" href='https://www.facebook.com/abdullah.tariq.7262'>
+                        <FaFacebook size={24} />
+                    </IconButton>
+                    <IconButton color="inherit" href="https://www.instagram.com/abdullahtariq62/?next=%2F&hl=en" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram size={24} />
+                    </IconButton>
+                    <IconButton color="inherit" target='blank' rel="noopener noreferrer" href='https://github.com/Abdullahprogramme'>
+                        <FaGithub size={24} />
+                    </IconButton>
+                </div>
+            </Paper>
             {/* Add more icons as needed */}
         </Box>
     );
